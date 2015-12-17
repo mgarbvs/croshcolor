2c964d793b9c2b781a73a3e0c3cf51f149101313
mgarbvs <michael.garbus@gmail.com>
2015-12-16 20:48:09 -0700

    Create prefs.js
    taken from:background-color: #000000 //R:0 G:0 B:0
    foreground-color: #919191 //R:145 G:145 B:145
    cursor-color:     #919191 //R:145 G:145 B:145
    color-palette-override: {"0":"#303030","
    1":"#a43261","2":"#006ca5",
    "3":"#007086","4":"#6751a6","5":"#913e88",
    "6":"#0061b1","7":"#c6c6c6","8":"#5e5e5e",
    "9":"#ff9fc9","10":"#3bd6ff","11":"#00ddf4","
    12":"#d5b8ff","13":"#ffa7f6","14":"#93c9ff","15":"#ffffff"}

---
+++ prefs.js
@@ -0,0 +1,26 @@
+// Disable bold. 
+term_.prefs_.set('enable-bold', false) 
+
+// Use this for Solarized Dark 
+term_.prefs_.set('background-color', "#000000");
+term_.prefs_.set('foreground-color', "#919191");
+term_.prefs_.set('cursor-color', "#919191");
+
+term_.prefs_.set('color-palette-overrides', [
+  '#303030',
+  '#a43261', 
+  '#007086', 
+  '#6751a6', 
+  '#913e88', 
+  '#0061b1', 
+  '#c6c6c6', 
+  '#5e5e5e', 
+  '#ff9fc9', 
+  '#3bd6ff', 
+  '#00ddf4', 
+  '#d5b8ff', 
+  '#ffa7f6', 
+  '#6c71c4', 
+  '#93c9ff', 
+  '#ffffff'
+]);
